dd410e8aeae9ce0b1cee859dc58161dff64fcb76
Ben Fagan <benjamin.s.fagan@gmail.com>
2025-07-10 20:01:28 -0700

    Refactored c&f .h & .cpp to put storage last to fix forward declaration errors.

--- containersAndFunctions.h
+++ containersAndFunctions.h
@@ -7,31 +7,6 @@
 
 namespace bankSimulation {
 	// ====================
-	// Storage Class
-	// ====================
-
-	class Storage {
-	private:
-		std::vector<Account> accounts;
-		std::vector<BankFunds> funds;
-		unsigned lastAccountNumber = 10'000;
-	public:
-		//Accessors
-		std::vector<Account>& getAccounts();
-		std::vector<BankFunds>& getFunds();
-
-		//Account Specific Functions
-		void newAccount();
-		void searchAccounts() const;
-		void saveAccount();
-		void loadAccount();
-
-		//Bank Save/Load Functions
-		void saveBank();
-		void loadBank();
-	};
-	
-	// ====================
 	// Account Class
 	// ====================
 
@@ -117,6 +92,31 @@ namespace bankSimulation {
 
 		//Print Function
 		void printBankReport() const;
+	};
+
+	// ====================
+	// Storage Class
+	// ====================
+
+	class Storage {
+	private:
+		std::vector<Account> accounts;
+		std::vector<BankFunds> funds;
+		unsigned lastAccountNumber = 10'000;
+	public:
+		//Accessors
+		std::vector<Account>& getAccounts();
+		std::vector<BankFunds>& getFunds();
+
+		//Account Specific Functions
+		void newAccount();
+		void searchAccounts() const;
+		void saveAccount();
+		void loadAccount();
+
+		//Bank Save/Load Functions
+		void saveBank();
+		void loadBank();
 	};
 	
 	// ====================
